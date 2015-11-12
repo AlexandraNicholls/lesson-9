a48b036e5b22aa0a76757c870d021e44d495e838
AlexandraNicholls <alex@nicholls.ws>
2015-11-12 11:43:02 -0500

    bower jquery!

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@
 
 //app.ts uses typescript, so we get CODE HINTING!!!
 //tsc app.ts in command line makes app.js
+if($(document).ready())
 
 var alex:objects.Person = new objects.Person("Alex");
 alex.sayName();
